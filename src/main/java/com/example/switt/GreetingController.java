d6d6ec394cb95c3f9c3b9dd762c61e5c20aae5b0
m.doronin <m.doronin@haulmont.com>
2021-01-23 21:19:10 +0400

    add db/repo/filter

--- src/main/java/com/example/switt/GreetingController.java
+++ src/main/java/com/example/switt/GreetingController.java
@@ -1,14 +1,20 @@
 package com.example.switt;
 
+import com.example.switt.domain.Message;
+import com.example.switt.repos.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepository messageRepository;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
@@ -17,11 +23,32 @@ public class GreetingController {
     }
 
     @GetMapping
-    public String main(@RequestParam(name = "text", required = false, defaultValue = "Default World") String text ,
-                       @RequestParam(name = "tag", required = false) String tag,
-                       Map<String, Object> model) {
-        model.put("name", text);
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
         return "main";
     }
 
+    @PostMapping
+    @SuppressWarnings(value = "unused")
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepository.save(message);
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    @SuppressWarnings(value = "unused")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.put("messages", messages);
+        return "main";
+    }
 }
---
+++ src/main/java/com/example/switt/repos/MessageRepository.java
@@ -0,0 +1,11 @@
+package com.example.switt.repos;
+
+import com.example.switt.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
