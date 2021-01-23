e3792b57647ffae147b79343660f64f48fbf9dd0
m.doronin <m.doronin@haulmont.com>
2021-01-23 23:36:26 +0400

    add spring-security, login page

--- src/main/java/com/example/switt/GreetingController.java
+++ src/main/java/com/example/switt/GreetingController.java
@@ -16,20 +16,21 @@ public class GreetingController {
     @Autowired
     private MessageRepository messageRepository;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    @SuppressWarnings(value = "unused")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
+    @SuppressWarnings(value = "unused")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     @SuppressWarnings(value = "unused")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
